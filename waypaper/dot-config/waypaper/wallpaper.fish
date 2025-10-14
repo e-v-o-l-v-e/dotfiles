fd3c0819c27793d47ec31379c1712c57751f1ce3
evolve <elivanoa5@gmail.com>
2025-10-14 21:29:54 +0200

    pre theme

--- waypaper/dot-config/waypaper/wallpaper.fish
+++ waypaper/dot-config/waypaper/wallpaper.fish
@@ -6,10 +6,10 @@ ln -sf $wp ~/.wallpaper
 
 matugen image ~/.wallpaper -m $THEME # -t scheme-fruit-salad # -t scheme-expressive
 
-if test $THEME = "light"
-  wal -i $wp -l
-else
-  wal -i $wp
-end
+# if test $THEME = "light"
+#   wal -i $wp -l
+# else
+#   wal -i $wp
+# end
 
 notify-send -i $wp "Theme changed" "Wallpaper has been updated"
